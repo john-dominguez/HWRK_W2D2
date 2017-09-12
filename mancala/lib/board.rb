dc405f398a3bb0b12afc7b230fd0b27aadb976f8
App Academy Student <appacademy@Apps-Mac-mini-2.local>
2017-09-11 20:21:49 -0400

    working on board.rb 16/17 specs

--- mancala/lib/board.rb
+++ mancala/lib/board.rb
@@ -2,16 +2,23 @@ class Board
   attr_accessor :cups
 
   def initialize(name1, name2)
+    @cups = Array.new(14){0}
+
   end
 
   def place_stones
     # helper method to #initialize every non-store cup with four stones each
+    @cups.each {|cup| cup.count = 4}
+    @cups[6].count = 0
+    @cups[13].count = 0
   end
 
   def valid_move?(start_pos)
   end
 
   def make_move(start_pos, current_player_name)
+
+    render
   end
 
   def next_turn(ending_cup_idx)
@@ -27,6 +34,9 @@ class Board
   end
 
   def one_side_empty?
+    top = @cups[7..12].all? {|cup| cup.count == 0}
+    bottom = @cups.take(6).all? {|cup| cup.count == 0}
+    top || bottom
   end
 
   def winner
